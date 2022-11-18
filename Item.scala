d7d429957fc7a207a4efec3c3250924a1dee2e9d
Zivular <94075612+Zivular@users.noreply.github.com>
2022-11-18 15:43:12 +0200

    Update Item.scala

--- Item.scala
+++ Item.scala
@@ -14,5 +14,16 @@ class Item(val name: String, val description: String):
   /** Returns a short textual representation of the item (its name, that is). */
   override def toString = this.name
 
+
+  object Knife:
+
+  end Knife
+
+  object WoodenPlank:
+
+  end WoodenPlank
+
+
+
 end Item
 

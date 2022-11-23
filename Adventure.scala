cb0728cb84af696776fab5002f602b7ea2865d9e
Zivular <94075612+Zivular@users.noreply.github.com>
2022-11-23 16:16:35 +0200

    Update Adventure.scala

--- Adventure.scala
+++ Adventure.scala
@@ -1,5 +1,7 @@
 package o1.adventure
 
+import o1.adventure.ui.AdventureTextUI.player
+
 /** The class `Adventure` represents text adventure games. An adventure consists of a player and
   * a number of areas that make up the game world. It provides methods for playing the game one
   * turn at a time and for checking the state of the game.
@@ -19,7 +21,7 @@ class Adventure:
   private val kamppi = Area("Station 4", "You arrive at Kamppi, there are some hobos near. They look friendly")
   private val ruoholahti = Area("Station 5", "You arrive at Ruoholahti, there seems to be a lot of water at the exit. You need to make a raft to continue")
   private val lauttasaari = Area("Station 6", "You arrive at Lauttasaari, you hear music upstairs.")
-  private val keilaniemi = Area("Station 7", "It's pitch black and you arrive at keilaniemi, it's eerie in here and you hear a loud yelling from the darkness infront of you")
+  private val keilaniemi = Area("Station 7", "It's pitch black and you arrive at keilaniemi, it's eerie in here and you hear a loud yelling from the darkness infront of you.\nA furious and intoxicated konealfa attacks you!")
   private val otaniemi = Area("Station 8", "You see a bright light, you fall on the floor.")
   private val destination = otaniemi
 
@@ -30,14 +32,9 @@ class Adventure:
   lauttasaari.setNeighbors(Vector("west" -> keilaniemi))
   keilaniemi.setNeighbors(Vector("west" -> otaniemi))
 
-  // TODO: Uncomment the two lines below. Improve the code so that it places the items in clearing and southForest, respectively.
-
-  clearing.addItem(Item("battery", "It's a small battery cell. Looks new."))
-  southForest.addItem(Item("remote", "It's the remote control for your TV.\nWhat it was doing in the forest, you have no idea.\nProblem is, there's no battery."))
-
-
   /** The character that the player controls in the game. */
   val player = Player(itakeskus)
+  this.keilaniemi.addMonster(Monster("Konealfa", 500, 33, 66))
 
 
   /** Determines if the adventure is complete, that is, if the player has won. */
@@ -46,17 +43,17 @@ class Adventure:
   /** Determines whether the player has won, lost, or quit, thereby ending the game. */
   def isOver = this.isComplete || this.player.hasQuit || this.player.isDead
 
+
   /** Returns a message that is to be displayed to the player at the beginning of the game. */
   def welcomeMessage = s"The year is 3000. There has been a nuclear war on the surface of earth and people live in Metro tunnels.\nYou are located in Itäkeskus, Helsinki, Finland and you need to go to Otaniemi, Espoo to meet your scientist friends."
-
 
 
   /** Returns a message that is to be displayed to the player at the end of the game. The message
     * will be different depending on whether or not the player has completed their quest. */
   def goodbyeMessage =
     if this.isComplete then
-      "\nzzzzzz\nzzzzzz\nzzzzzz\nWake up Mr. Freeman. Wake up and smell the ashes"
-    else if this.turnCount == this.timeLimit then
+      "\nzzzzzz\nzzzzzz\nzzzzzz\nWake up Mr. Freeman. Wake up and smell the ashes\nYou won!"
+    else if this.player.isDead then
       "You died.\nGame over!"
     else  // game over due to player quitting
       "Quitter!"
@@ -66,11 +63,22 @@ class Adventure:
     * report of what happened, or an error message if the command was unknown. In the latter
     * case, no turns elapse. */
   def playTurn(command: String) =
+    if player.location.returnMonster.nonEmpty then {
+      fight(this.player.location.returnMonster(0), this.player.location)
+    }
     val action = Action(command)
     val outcomeReport = action.execute(this.player)
-    if outcomeReport.isDefined then
-      this.turnCount += 1
     outcomeReport.getOrElse(s"Unknown command: \"$command\".")
+
+  def fight(monster: Monster, area: Area) = {
+   if monster.currentHealth <= 0 then
+     println(s"You killed the ${monster.name}!")
+     area.killMonster()
+   else
+      val monsterDamage = monster.attack(monster.baseAttackPower)
+      if damageTaken != 0 then println(s"The monster strikes you! You lose $monsterDamage health") else println(s"The monster misses!")
+      player.takeDamage(monsterDamage)
+  }
 
 end Adventure
 

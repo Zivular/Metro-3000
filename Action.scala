d78a79f80dd56e959ad479b2cc1cbaea84fdc4c5
Zivular <94075612+Zivular@users.noreply.github.com>
2022-11-23 16:18:36 +0200

    Update Action.scala

--- Action.scala
+++ Action.scala
@@ -7,25 +7,46 @@ package o1.adventure
 class Action(input: String):
 
   private val commandText = input.trim.toLowerCase
-  private val verb        = commandText.takeWhile( _ != ' ' )
-  private val modifiers   = commandText.drop(verb.length).trim
+  private val verb       = commandText.takeWhile( _ != ' ' )
+  private val modifiers  = commandText.drop(verb.length).trim
 
   /** Causes the given player to take the action represented by this object, assuming
     * that the command was understood. Returns a description of what happened as a result
     * of the action (such as “You go west.”). The description is returned in an `Option`
     * wrapper; if the command was not recognized, `None` is returned. */
-  def execute(actor: Player) = 
+  def execute(actor: Player) =
     if actor.location.returnMonster.isEmpty then
       this.verb match
       case "go"        => Some(actor.go(this.modifiers))
+
       case "rest"      => Some(actor.rest())
+
       case "xyzzy"     => Some("The grue tastes yummy.")
+
       case "quit"      => Some(actor.quit())
+
       case "get"       => Some(actor.get(this.modifiers))
+
       case "drop"      => Some(actor.drop(this.modifiers))
+
       case "examine"   => Some(actor.examine(this.modifiers))
+
       case "inventory" => Some(actor.inventory)
+
       case other       => None
+    else this.verb match
+
+      case "attack" => Some(actor.attack)
+
+      case "defence"  => Some("Joku")
+
+      case "counter" => Some("choose attack")
+
+      case "weapon" => Some("jgej")
+
+      case "quit" => Some(actor.quit())
+
+      case other => None
 
   /** Returns a textual description of the action object, for debugging purposes. */
   override def toString = s"$verb (modifiers: $modifiers)"

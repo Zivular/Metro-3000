ade2f91bc5966670163f726c880b68e1fad4b9ee
Zivular <94075612+Zivular@users.noreply.github.com>
2022-11-26 22:10:29 +0200

    Update Character.scala

--- Character.scala
+++ Character.scala
@@ -1,8 +1,14 @@
 package o1.adventure
 
-class Character(val name: String, val health: Int, val shop: Map[String, Item]):
+import scala.collection.mutable.Map
 
+class Character(val name: String, val health: Int, val item: Map[String, Item], val wantedItem: String):
 
+  private val ownedItem = item
+
+  def currentOwnedItem = this.ownedItem
+
+  def addItem(item: Item) = this.ownedItem.put(item.name, item)
 
 
 end Character
